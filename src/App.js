7041dc4898a333595074ad9d3fa2d353d590e7cf
WildCodeSchool <anelisynagano@gmail.com>
2020-10-16 21:24:16 +0100

    start repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import here Radium from radium library
+
 
 function App() {
+  // create a variable styles and create a stylesheet for the following classes and apply the properties:
+  // - list: display: flex; list-style-type: none; (tip: don't forget to camelCase the property!)
+  // - title: color: #39424e;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // add the correct attribute to your ul and h1 tags, and assign the correct values
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <h1>Radium!</h1>
+      <p>This is an example of Radium</p>
     </div>
   );
 }
 
+// don't forget to wrap your component with the Radium function!
 export default App;
